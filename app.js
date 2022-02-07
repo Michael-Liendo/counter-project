148275b55c4d513655d28ee954d40ded0da96bc2
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-02-06 20:39:42 -0400

    feat(counter):
    the counter has functionality

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+let counterDiv = document.querySelector("#counter");
+let counter = 0;
+let btDecrease = document.querySelector("#btDecrease");
+let btIncrease = document.querySelector("#btIncrease");
+
+// Add event listeners
+btDecrease.addEventListener("click", decrease);
+btIncrease.addEventListener("click", increase);
+
+// Functions
+function decrease() {
+  counter--;
+  counterDiv.innerHTML = counter;
+}
+
+function increase() {
+  counter++;
+  counterDiv.innerHTML = counter;
+}
+
+counterDiv.innerHTML = counter;
